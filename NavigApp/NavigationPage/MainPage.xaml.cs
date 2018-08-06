017c397db84a07451de3e03d021856e657ffd0d6
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-08-06 16:00:35 +0700

    Create MainPage.xaml.cs

---
+++ NavigApp/NavigationPage/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace NavigApp
+{
+       public partial class MainPage : ContentPage
+    {
+       public  MainPage()
+       {
+             InitializeComponent();                            
+        }
+        async private void nextPage(object sender, EventArgs e)
+        {
+         await Navigation.PushAsync(new SecondPage());
+        }
+
+    }
+}
