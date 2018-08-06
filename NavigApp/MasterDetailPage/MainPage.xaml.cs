7d08677c2d5ed0762ee8c7e35e806d9844d94dfb
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-08-06 10:15:43 +0700

    Create MainPage.xaml.cs

---
+++ NavigApp/MasterDetailPage/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace NavigApp
+{
+       public partial class MainPage : MasterDetailPage
+    {
+              public  MainPage()
+              {
+                     InitializeComponent();                     
+              }
+       }
+}
