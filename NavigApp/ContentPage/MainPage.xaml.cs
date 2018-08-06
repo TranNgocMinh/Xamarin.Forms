4ee9c554d955c28a00c585894b228a2bf67e01b3
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-08-06 09:19:02 +0700

    Create MainPage.xaml.cs

---
+++ NavigApp/ContentPage/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace NavigApp
+{
+       public partial class MainPage : ContentPage
+       {
+              public  MainPage()
+              {
+                     InitializeComponent();                    
+              }
+       }
+}
